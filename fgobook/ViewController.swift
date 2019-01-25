01b6d22491126ebc192d4e13455e9bd65269ce0e
g87736954 <g87736954@gmail.com.tw>
2019-01-25 11:56:40 +0800

    Initial Commit

---
+++ fgobook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fgobook
+//
+//  Created by User on 2019/1/25.
+//  Copyright © 2019 g87. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
